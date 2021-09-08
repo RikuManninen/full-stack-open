837955a9fc1a228a44eeaaa304fd5fbc15029e2a
rikuma0 <riku.manninen@icloud.com>
2021-09-08 17:47:09 +0300

    exercise 2.12

---
+++ part2/countries/src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const App = () => {
+
+  const [countries, setCountries] = useState([])
+  const [searchWord, setSearchWord] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => setCountries(response.data))
+  })
+
+  const countriesToShow = searchWord
+    ? countries.filter(c => c.name.toLowerCase().includes(searchWord.toLowerCase()))
+    : []
+
+  const handleSearchChange = (event) => {
+    setSearchWord(event.target.value)
+  }
+
+  return (
+    <>
+      find countries <input
+        value={searchWord}
+        onChange={handleSearchChange}
+      />
+      {
+        countriesToShow.length === 1 
+          ? <div>
+            <h1>{countriesToShow[0].name}</h1>
+            <p>capital {countriesToShow[0].capital}</p>
+            <p>population {countriesToShow[0].population}</p>
+            <h2>languages</h2>
+            <ul>
+              {countriesToShow[0].languages.map(lang => <li key={lang.name}>{lang.name}</li>)}
+            </ul>
+            <img src={countriesToShow[0].flag} alt={'flag'} height={'150'}/>
+          </div>
+
+        : countriesToShow.length <= 10 
+          ? countriesToShow.map(country => <div key={country.name}>{country.name}</div>)
+
+        : <div>Too many matches, specify another filter</div>
+      }
+    </>
+  )
+}
+
+export default App;
