0f6e5f2a8d3bfe3435f68dcd9399a3a32ef1ebf5
Riku Manninen <riku.manninen@icloud.com>
2021-12-01 12:25:16 +0200

    exercise 4.17

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   res.json(blogs.map(blog => blog.toJSON()))
 })
 
@@ -14,14 +16,19 @@ blogsRouter.get('/:id', async (req, res) => {
 blogsRouter.post('/', async (req, res) => {
   const body = req.body
 
+  const user = await User.findOne()
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes | 0
+    likes: body.likes | 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   res.json(savedBlog.toJSON())
 })
 
