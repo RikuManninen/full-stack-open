db07589eaa091c44673dcc9b59497f1861c821c8
Riku Manninen <riku.manninen@icloud.com>
2022-01-15 13:36:42 +0200

    exercise 5.6

--- part5/src/App.js
+++ part5/src/App.js
@@ -3,6 +3,7 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login' 
 import Notification from './components/Notification'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -12,13 +13,7 @@ const App = () => {
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
 
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
-
   const [ message, setMessage ] = useState(null)
-
-  const [blogFormVisible, setBlogFormVisible] = useState(false)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -34,6 +29,11 @@ const App = () => {
       blogService.setToken(user.token)
     }
   }, [])
+
+  const msg = (obj) => {
+    setMessage(obj)
+    setTimeout(() => setMessage(null), 5000)
+  }
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -66,34 +66,6 @@ const App = () => {
     console.log('logged out')
   }
 
-  const addBlog = (event) => {
-    event.preventDefault()
-    const blogObject = {
-      title: title,
-      author: author,
-      url: url
-    }
-
-    blogService
-      .create(blogObject)
-        .then(returnedBlog => {
-          setBlogs(blogs.concat(returnedBlog))
-          msg({
-            type: 'success',
-            content: `a new blog ${returnedBlog.title} by ${returnedBlog.author} added`
-          })
-          setTitle('')
-          setAuthor('')
-          setUrl('')
-          setBlogFormVisible(false)
-        })
-  }
-
-  const msg = (obj) => {
-    setMessage(obj)
-    setTimeout(() => setMessage(null), 5000)
-  }
-
   const loginForm = () => {
     return (
       <div>
@@ -123,59 +95,13 @@ const App = () => {
     )
   }
 
-  const blogForm = () => {
-    const hideWhenVisible = { display: blogFormVisible ? 'none' : '' }
-    const showWhenVisible = { display: blogFormVisible ? '' : 'none' }
-    return (
-      <div>
-        <div style={hideWhenVisible}>
-          <button onClick={() => setBlogFormVisible(true)}>create new blog</button>
-        </div>
-        <div style={showWhenVisible}>
-          <h2>create new</h2>
-          <form onSubmit={addBlog}>
-            <div>
-              title:
-                <input
-                type="text"
-                value={title}
-                name="Title"
-                onChange={({ target }) => setTitle(target.value)}
-              />
-            </div>
-            <div>
-              author:
-                <input
-                type="text"
-                value={author}
-                name="Author"
-                onChange={({ target }) => setAuthor(target.value)}
-              />
-            </div>
-            <div>
-              url:
-                <input
-                type="text"
-                value={url}
-                name="Url"
-                onChange={({ target }) => setUrl(target.value)}
-              />
-            </div>
-            <button type="submit">create</button>
-          </form>
-          <button onClick={() => setBlogFormVisible(false)}>cancel</button>
-        </div>
-      </div>
-    )
-  }
-
   const blogView = () => {
     return (
       <div>
         <h2>blogs</h2>
         <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
 
-        {blogForm()}
+        <BlogForm blogService={blogService} blogs={blogs} setBlogs={setBlogs} msg={msg} />
 
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
---
+++ part5/src/components/BlogForm.js
@@ -0,0 +1,79 @@
+import React, {useState} from "react"
+const BlogForm = ({ blogService, blogs, setBlogs, msg }) => {
+  
+  const [blogFormVisible, setBlogFormVisible] = useState(false)
+
+  const hideWhenVisible = { display: blogFormVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogFormVisible ? '' : 'none' }
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: title,
+      author: author,
+      url: url
+    }
+
+    blogService
+      .create(blogObject)
+        .then(returnedBlog => {
+          setBlogs(blogs.concat(returnedBlog))
+          msg({
+            type: 'success',
+            content: `a new blog ${returnedBlog.title} by ${returnedBlog.author} added`
+          })
+          setTitle('')
+          setAuthor('')
+          setUrl('')
+          setBlogFormVisible(false)
+        })
+  }
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setBlogFormVisible(true)}>create new blog</button>
+      </div>
+      <div style={showWhenVisible}>
+        <h2>create new</h2>
+        <form onSubmit={addBlog}>
+          <div>
+            title:
+              <input
+              type="text"
+              value={title}
+              name="Title"
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </div>
+          <div>
+            author:
+              <input
+              type="text"
+              value={author}
+              name="Author"
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </div>
+          <div>
+            url:
+              <input
+              type="text"
+              value={url}
+              name="Url"
+              onChange={({ target }) => setUrl(target.value)}
+            />
+          </div>
+          <button type="submit">create</button>
+        </form>
+        <button onClick={() => setBlogFormVisible(false)}>cancel</button>
+      </div>
+    </div>
+  )
+}
+
+export default BlogForm
