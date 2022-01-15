e9b07afce68a00cb59bf108506f01e33ea96b253
Riku Manninen <riku.manninen@icloud.com>
2022-01-15 13:50:50 +0200

    exercise 5.7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,30 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [blogVisible, setBlogVisible] = useState(false)
+  const hideWhenVisible = { display: blogVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogVisible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author} <button onClick={() => setBlogVisible(true)}>show</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title} <button onClick={() => setBlogVisible(false)}>hide</button><br/>
+        {blog.url}<br/>
+        likes {blog.likes} <button>like</button><br/>
+        {blog.author}
+      </div>
+    </div>  
+  )
+  }
 
 export default Blog
