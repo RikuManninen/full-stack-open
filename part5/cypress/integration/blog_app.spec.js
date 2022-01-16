b4cdc382797aa601c22e47d57209f56bc6aa17df
Riku Manninen <riku.manninen@icloud.com>
2022-01-16 14:11:45 +0200

    exercise 5.18

--- part5/cypress/integration/blog_app.spec.js
+++ part5/cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Note app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Riku Manninen',
+      username: 'riku',
+      password: 'salasana'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +15,21 @@ describe('Note app', function() {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('input:first').type('riku')
+      cy.get('input:last').type('salasana')
+      cy.contains('login').click()
+      cy.contains('Riku Manninen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('input:first').type('riku')
+      cy.get('input:last').type('salasana2')
+      cy.contains('login').click()
+      cy.contains('wrong username or password').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
