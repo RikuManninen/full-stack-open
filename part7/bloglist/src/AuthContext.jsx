e46a92c5632bbfcea35044404de397a370724de6
Riku Manninen <38488460+RikuManninen@users.noreply.github.com>
2024-02-07 03:22:52 +0200

    exercise 7.13

--- part7/bloglist/src/AuthContext.jsx
+++ part7/bloglist/src/AuthContext.jsx
@@ -1,29 +1,41 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useReducer } from "react";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 
 const AuthContext = React.createContext();
 
-export const useAuth = () => useContext(AuthContext);
+const initialState = {
+  user: null,
+};
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    default:
+      return state;
+  }
+};
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(() => {
+  const [state, dispatch] = useReducer(authReducer, initialState, () => {
     const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON);
       blogService.setToken(user.token);
-      return user;
+      return { user };
     }
-    return null;
+    return initialState;
   });
 
-  console.log("user state has been updated", user);
   const login = async ({ username, password }) => {
     try {
       const user = await loginService.login({ username, password });
       window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
       blogService.setToken(user.token);
-      setUser(user);
+      dispatch({ type: "LOGIN", payload: user });
     } catch (error) {
       console.error("Login error:", error);
     }
@@ -32,12 +44,14 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     window.localStorage.removeItem("loggedBlogappUser");
     blogService.setToken(null);
-    setUser(null);
+    dispatch({ type: "LOGOUT" });
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ ...state, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+export const useAuth = () => useContext(AuthContext);
