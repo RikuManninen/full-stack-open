0b9380a22148c35fd23de7d5fd3d11abf96f24cd
Riku Manninen <38488460+RikuManninen@users.noreply.github.com>
2024-06-10 10:20:42 +0300

    exercise 8.3

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -106,10 +106,16 @@ const typeDefs = `
     genres: [String!]!
   }
 
+  type Author {
+    name: String!
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -117,7 +123,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => books.filter((book) => book.author === root.name).length
   }
 }
 
