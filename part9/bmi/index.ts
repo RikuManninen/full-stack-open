2430dd8b3b0bda6508d4baba8b08e7651d802f7f
Riku Manninen <38488460+RikuManninen@users.noreply.github.com>
2024-06-12 03:26:08 +0300

    exercise 9.5

--- part9/bmi/index.ts
+++ part9/bmi/index.ts
@@ -1,8 +1,28 @@
 import express from 'express';
+import qs from 'qs';
+import calculateBmi from './bmiCalculator';
+import { isNotNumber } from './utils';
+
 const app = express();
+
+app.set('query parser',
+  (str: string) => qs.parse(str));
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query;
+
+  if (isNotNumber(height) || isNotNumber(weight)) {
+    res.send({error: 'malformatted parameters'});
+    return;
+  }
+
+  const result = calculateBmi(Number(height), Number(weight));
+
+  res.send({weight: Number(weight), height: Number(height), bmi: result});
 });
 
 const PORT = 3003;
